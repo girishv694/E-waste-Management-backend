ebdd54f96e6fb5a4a5db1cb8e5e3920b1432e5b5
Girishv <girishv694@gmail.com>
2021-11-10 15:17:17 +0530

    login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,11 +1,39 @@
 const express = require('express')
-const logins = require('../models/user.model')
+const user = require('../models/user.model')
 const router = express.Router();
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+
+router.post('/register',async (req,res)=>{
+    // const newPassword = await bcrypt.hash(req.body.password,10)
+    const log = await user.create({
+        username : req.body.username,
+        email : req.body.email,
+        password : req.body.password
+    })
+    res.status(201).send({log});
+})
+
 
 
 router.post('/signin',async (req,res)=>{
-    const log = await logins.create(req.body)
-    res.status(201).send({log});
+    const User = await user.findOne({
+        email : req.body.email,
+        password : req.body.password,
+    })
+
+    if(User){
+        const token  = jwt.sign({
+         username : User.username,
+         email : User.email,
+        },
+        'secret123')
+
+        return res.json({status:'ok',User : token})
+        console.log(token)
+    }
+
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const connect = require('./config/db')
 const login = require('./controllers/user.controller')
+const cors = require('cors')
 
 
 const app = express();
 app.use(express.json());
 
-app.use('/',login)
+app.use(cors())
+
+app.use('/api',login)
 
 
 app.listen(3001, async() =>{
