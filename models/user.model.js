01b755b10dcc107646557912f805500aacc35e0d
Girishv <girishv694@gmail.com>
2021-11-12 10:22:38 +0530

    validation

--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose')
 
 const loginSchema = new mongoose.Schema({
-    username : {type:String},
-    email : {type:String},
-    password : {type:String}
+    username : {type:String,required:true},
+    email : {type:String,required:true},
+    password : {type:String,required:true},
+    // tokens :[{
+    //     token : {
+    //         type:String,
+    //         required:true
+    //     }
+    // }]
+},{
+    timestamps:true
 })
 
 const logins = mongoose.model('user',loginSchema);
