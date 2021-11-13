53295d330aea61ec8bb4e51026a91099f3005921
Shivang <iamshivang@outlook.com>
2021-11-13 14:40:34 +0530

    ver

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -21,20 +21,18 @@
 // const validateLogin = require('../validation/login')
 // const verifyToken = require('../controllers/middlewares/verifyToken')
 
-// router.get('/getNum/:p', async (req, res) => {
-//   const arb = await User.findById(req.params.p)
-//   return res.send(arb.phone)
-// })
-
+// // router.get('/getNum/:p', async (req, res) => {
+// //   const arb = await User.findById(req.params.p)
+// //   return res.send(arb.phone)
+// // })
 
 // router.post("/register", (req, res) => {
-    
+
 //     //Form validation
-    
+
 //       if(Validator.isEmpty(req.body.username)) {
 //         res.status(400).json({message: "Name field is required"});
 //     }
-
 
 //     if(Validator.isEmpty(req.body.email)){
 //         res.status(400).json({message: "Email field is required"});
@@ -74,7 +72,7 @@
 //         phone: req.body.phone,
 //       })
 
-      // Hash password before storing in database
+// Hash password before storing in database
 //       const rounds = 10
 //       bcrypt.genSalt(rounds, (err, salt) => {
 //         bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -91,7 +89,7 @@
 // })
 
 // router.post('/signin', (req, res) => {
-  //Form Valdiation
+//Form Valdiation
 
 // //Password checks
 // if(Validator.isEmpty(req.body.password)){
@@ -101,7 +99,6 @@
 // if(!Validator.isLength(req.body.password,{min:6,max:30})){
 //     res.status(400).json({message:"Password must be at least 6 characters"}) ;
 // }
-
 
 //     User.findOne({email:req.body.email}).then(user=>{
 
@@ -132,9 +129,6 @@
 
 // });
 
-
-
-
 //   if (Validator.isEmpty(req.body.email)) {
 //     return res.status(400).json({ message: 'Email field is required' })
 //   } else if (!Validator.isEmail(req.body.email)) {
@@ -146,31 +140,24 @@
 //     return res.status(400).json({ message: 'Password field is required' })
 //   }
 
-
-
 // router.post("/signin",(req,res) => {
 
 //     //Form Valdiation
-   
+
 //     if(Validator.isEmpty(req.body.email)){
 //         return res.status(400).json({message:"Email field is required"});
 //     } else if(!Validator.isEmail(req.body.email)){
 //         return res.status(400).json({message:"Email is invalid"});
 //     }
 
-
-
 //     //Password checks
 //     if(Validator.isEmpty(req.body.password)){
 //         return res.status(400).json({message : "Password field is required"})
 //     }
 
-
-
-
 //     const email = req.body.email;
 //     const password = req.body.password;
-   
+
 //     //Find user by Email
 //     User.findOne({email}).then(user=>{
 //         if(!user){
@@ -185,7 +172,7 @@
 //             const token  = jwt.sign({
 //                 username : user.username,
 //                 email : user.email,
-               
+
 //                },
 //                'secret123',{
 //                    expiresIn:'1h'
@@ -197,15 +184,10 @@
 //             //        httpOnly:true
 //             //    }).send(token);
 
-       
 //                return res.json({status:'ok',user :{_id,username,email} ,token}).header('auth-token',token)
-                  
-            
-               
-//                console.log(token)
-            
 
-            
+//                console.log(token)
+
 //         }
 //         else{
 //             return res.json({message:'password incorrect', user:false})
@@ -213,8 +195,6 @@
 //       });
 //     });
 // });
-
-
 
 // router.post('/profile',verifyToken,(req,res)=>{
 //     res.status(200).json({user :"profile"})
@@ -224,89 +204,80 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
 const mongoose = require('mongoose')
 const express = require('express')
-const router = express.Router();
+const router = express.Router()
 const User = require('../models/user.model')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const authenticate = require('../middlewares/authenticate')
 
-router.post("/register",async (req,res)=>{
-
-  const {username , email , password,phone} = req.body;
-
-  if(!username || !email || !password || !phone){
-    return res.status(400).json({error:"please fill all the details"})
-  }
-
-  try{
-    const exist = await User.findOne({email:email});
-    if(exist){
-      return res.status(400).json({error : "Email already exists"})
-    }
-    else{
-      const user = new User ({ username,email,password,phone});
-       await user.save()
-
-      res.status(201).json({message:"user registered successfully"})
-      
-    }
-
-  }
-  catch(err){
-    return res.status(400).json({error : "Failed"})
-
-  }
-
-});
-
-
-
-router.post('/login',async (req,res)=>{
- const {email ,password} = req.body;
-
- try{
-
-  if(!email || !password){
-    return res.status(400).json({error : "Please fill all fields"})
-  }
-
-  const userLogin = await User.findOne({email:email});
-  if(userLogin){
-    const isMatch = await bcrypt.compare(password,userLogin.password)
-
-    const token = await userLogin.generateToken()
-
-    res.cookie("girishv",token, {
-      expires : new  Date(Date.now() + 25892000000),
-      httpOnly:true
-    });
-
-    if(!isMatch){
-      return res.status(400).json({error : "Invalid credentials"})
-    }
-    else{
-      return res.json({message : "user signed in successfully"})
-    }
-    
-  }
-  else{
-    return res.json({error : "Email does not exists"})
-  }
-}
-
-
- catch(err){
-   console.log(err)
- }
- 
+router.get('/getNum/:p', async (req, res) => {
+  // const ar = await User.find()
+  // console.log(ar);
+  const arb = await User.findById(req.params.p)
+  return res.send(arb)
 })
 
+router.post('/register', async (req, res) => {
+  const { username, email, password, phone } = req.body
 
-router.get('/about',authenticate,(req,res)=>{
+  if (!username || !email || !password || !phone) {
+    return res.status(400).json({ error: 'please fill all the details' })
+  }
+
+  try {
+    const exist = await User.findOne({ email: email })
+    if (exist) {
+      return res.status(400).json({ error: 'Email already exists' })
+    } else {
+      const user = new User({ username, email, password, phone })
+      await user.save()
+
+      res.status(201).json({ message: 'User registered successfully' })
+    }
+  } catch (err) {
+    return res.status(400).json({ error: 'Failed' })
+  }
+})
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ error: 'Please fill all fields' })
+    }
+
+    const userLogin = await User.findOne({ email: email })
+    if (userLogin) {
+      const isMatch = await bcrypt.compare(password, userLogin.password)
+
+      const token = await userLogin.generateToken()
+
+      res.cookie('girishv', token, {
+        expires: new Date(Date.now() + 25892000000),
+        httpOnly: true,
+      })
+
+      if (!isMatch) {
+        return res.status(400).json({ error: 'Invalid credentials' })
+      } else {
+        return res.json({
+          message: 'user signed in successfully',
+          user: userLogin.phone,
+        })
+      }
+    } else {
+      return res.json({ error: 'Email does not exists' })
+    }
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+router.get('/about', authenticate, (req, res) => {
   res.send(req.rootUser)
 })
 
-module.exports = router;
+module.exports = router
