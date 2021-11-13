630addecfeddf1c03ba691ababaff846683e2968
Girishv <girishv694@gmail.com>
2021-11-13 10:11:19 +0530

    vali

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-const express = require("express")
-const connect = require('./config/db')
-const login = require('./controllers/user.controller')
-const productController = require('./controllers/product.controller')
 
+ const productController = require('./controllers/product.controller')
+const mongoose = require('mongoose')
+const express = require('express')
+const connect = require('./config/db')
+const app = express();
+const user = require('./controllers/user.controller')
 const cors = require('cors')
 const category = require('./controllers/Category.controller')
-const ac = require('./controllers/Ac.controller')
 
 
-const app = express();
+
 app.use(express.json());
-
 app.use(cors())
 
-app.use('/user',login)
+app.use("/user",user)
 app.use('/products', productController)
-app.use('/category',category)
-// app.use('/ac',ac);
 
 
-app.listen(3001, async() =>{
+
+
+app.listen(3002, async() =>{
     await connect();
-    console.log("DB connected and running on port 3001");
+    console.log("DB connected and running on port 3002");
 })
+
