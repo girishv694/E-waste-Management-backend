2ef7dd48deeed8cc8f2ec54afc57b5c1402434ae
Girishv <girishv694@gmail.com>
2021-11-13 09:19:11 +0530

    validation

--- models/user.model.js
+++ models/user.model.js
@@ -1,19 +1,59 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
-const loginSchema = new mongoose.Schema({
-    username : {type:String,required:true},
-    email : {type:String,required:true},
-    password : {type:String,required:true},
-    // tokens :[{
-    //     token : {
-    //         type:String,
-    //         required:true
-    //     }
-    // }]
+const userSchema = new mongoose.Schema({
+    username : {
+                type:String,
+                required:true},
+    email : {
+             type:String,
+             required:true},
+    password : {
+                type:String,
+                required:true
+    } ,
+    phone : {
+        type:Number,
+        required:true
+    },
+    tokens : [{
+        token:{
+            type:String,
+            required:true
+        }
+    }]        
 },{
-    timestamps:true
+    timestamps : true
 })
 
-const logins = mongoose.model('user',loginSchema);
 
-module.exports = logins;
+
+
+// hashing passwords
+
+userSchema.pre('save',async function (next) {
+    if(this.isModified('password')){
+        this.password = await bcrypt.hash(this.password,12);
+
+    }
+    next();
+})
+
+
+userSchema.methods.generateToken = async function () {
+    try{
+        let token = jwt.sign({_id:this._id},'secret123');
+        this.tokens = this.tokens.concat({token:token})
+        await this.save();
+        return token;
+
+    } catch(err){
+        console.log("error")
+    }
+}
+
+
+const User = mongoose.model('user',userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,22 +1,48 @@
-const express = require("express")
+// const express = require("express")
+// const connect = require('./config/db')
+// const login = require('./controllers/user.controller')
+// const cors = require('cors')
+
+// const ac = require('./controllers/Ac.controller')
+
+
+// const app = express();
+// app.use(express.json());
+
+// app.use(cors())
+
+// app.use('/api',login)
+ 
+// app.use('/ac',ac);
+
+
+// app.listen(3001, async() =>{
+//     await connect();
+//     console.log("DB connected and running on port 3001");
+// })
+
+////////////////////////////////////////////////////////////////////////
+
+const mongoose = require('mongoose')
+const express = require('express')
 const connect = require('./config/db')
-const login = require('./controllers/user.controller')
+const app = express();
+const user = require('./controllers/user.controller')
 const cors = require('cors')
 const category = require('./controllers/Category.controller')
-const ac = require('./controllers/Ac.controller')
+app.use('/category',category)
 
 
-const app = express();
 app.use(express.json());
-
 app.use(cors())
 
-app.use('/api',login)
-app.use('/category',category)
-app.use('/ac',ac);
+app.use("/user",user)
 
 
-app.listen(3001, async() =>{
+
+
+app.listen(3002, async() =>{
     await connect();
-    console.log("DB connected and running on port 3001");
+    console.log("DB connected and running on port 3002");
 })
+
